b1ed6b7f4a67e5026397b7b099444e6cfe618c72
Rafiq <aazztech6@gmail.com>
2019-06-03 11:57:57 +0600

    initial commit

---
+++ directoirst-child-base.php
@@ -0,0 +1,14 @@
+<?php
+/**
+Plugin Name: Directorist Child
+Plugin URI: https://aazztech.com/product/directorist-child
+Description: This is an extension for Directorist plugin. It allows you to customize directorist by hooking.
+Version: 1.0.0
+Author: AazzTech
+Author URI: https://aazztech.com
+License: GPLv2 or later
+Text Domain: directorist-child
+Domain Path: /languages
+ */
+// prevent direct access to the file
+defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
